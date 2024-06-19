d653a1edc97c6e3c4c51af44154dff4b786d9884
Henry Fraser <henrymfraser@gmail.com>
2024-06-19 20:03:38 +1000

    refactor: split tests into testing one thing

--- __tests__/api/message.test.ts
+++ __tests__/api/message.test.ts
@@ -1,29 +1,47 @@
 import {
-  CodyClient,
-  SendMessageCommand,
-  SendMessageCommandInput, SendMessageCommandOutput
+    CodyClient,
+    SendMessageCommand,
+    SendMessageCommandInput,
+    SendMessageCommandOutput
 } from "@renfraser/cody-client";
 import {codyConfig} from "../../config";
 
-describe('Endpoint 1 Tests', () => {
-  it('should return valid response', async () => {
+describe('SendMessage', () => {
+    it('should return the expected status code response', async () => {
 
+        const client = new CodyClient(codyConfig);
 
-      const client = new CodyClient(codyConfig);
+        const messageText = "Hi!"
 
-      const messageText = "Hi!"
+        const params: SendMessageCommandInput = {
+            model: "HAIKU",
+            messages: [{
+                text: messageText
+            }]
+        }
+        const command = new SendMessageCommand(params);
 
-      const params: SendMessageCommandInput = {
-          model: "HAIKU",
-          messages: [{
-              text: messageText
-          }]
-      }
-      const command = new SendMessageCommand(params);
+        const data: SendMessageCommandOutput = await client.send(command);
 
-      const data: SendMessageCommandOutput = await client.send(command);
+        expect(data.$metadata.httpStatusCode).toBe(200);
+    });
 
-      expect(data.$metadata.httpStatusCode).toBe(200);
-      expect(data.messages[0].text).toBe(messageText);
-  });
+    it('should return the message history', async () => {
+
+        const client = new CodyClient(codyConfig);
+
+        const messageText = "Hi!"
+
+        const params: SendMessageCommandInput = {
+            model: "HAIKU",
+            messages: [{
+                text: messageText
+            }]
+        }
+        const command = new SendMessageCommand(params);
+
+        const data: SendMessageCommandOutput = await client.send(command);
+
+        expect(data.messages[0].text).toBe(messageText);
+    });
 });
